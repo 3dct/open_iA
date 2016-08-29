9640f054a13ba68fb2787ad6f17b372453e916bb
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2016-08-29 08:47:56 +0200

    Fix lower/upper case issue

--- core/src/iAStringHelper.h
+++ core/src/iAStringHelper.h
@@ -3,6 +3,6 @@
 class QString;
 class QStringList;
 
-#include "open_iA_core_export.h"
+#include "open_iA_Core_export.h"
 
 open_iA_Core_API QStringList SplitPossiblyQuotedString(QString const & additionalArguments);
