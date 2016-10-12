fcf9f170b6519e5db1a7f216a1f2cb880e5d9935
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2016-10-12 18:13:52 +0200

    CTest: fix project name

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,11 +1,14 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
+
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "open_iA")
+
+set(CTEST_PROJECT_NAME "openiA")
 set(CTEST_NIGHTLY_START_TIME "00:01:00 CEST")
+
 set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "git.3dct.at")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=openiA")
