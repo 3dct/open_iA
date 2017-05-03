2c1e82bc4341e74ad05745f0851edb3ec704983d
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2017-05-03 08:48:04 +0200

    CMake/CPack: set RUNPATH of platforms so correctly

--- cmake/ChangeRPath.sh
+++ cmake/ChangeRPath.sh
@@ -2,8 +2,10 @@
 
 # echo "Patching all .so files in $(pwd)"
 # for file in *.so*; do patchelf --set-rpath '$ORIGIN' $file; done
-for file in $(find . -name '*.so*')
+for file in *.so*
 do
 	# echo "Patching runpath in $file"
 	patchelf --set-rpath '$ORIGIN' $file
 done
+
+patchelf --set-rpath '$ORIGIN/..' platforms/libqxcb.so
