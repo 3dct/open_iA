48856b0de3ec769ec69d9178a53e07213fd5550d
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2017-08-02 21:23:35 +0200

    ASTRA: Dimension fix?

--- modules/AstraReconstruction/iAAstraAlgorithm.cpp
+++ modules/AstraReconstruction/iAAstraAlgorithm.cpp
@@ -400,7 +400,7 @@ void iAAstraAlgorithm::BackProject(AlgorithmType type)
 	auto volImg = AllocateImage(VTK_FLOAT, m_volDim, m_volSpacing);
 	FOR_VTKIMG_PIXELS(volImg, x, y, z)
 	{
-		volImg->SetScalarComponentFromFloat(x, y, z, 0, volumeData->getData3D()[z][x][y]);
+		volImg->SetScalarComponentFromFloat(x, y, z, 0, volumeData->getData3D()[z][y][x]);
 	}
 	getConnector()->SetImage(volImg);
 	getConnector()->Modified();
