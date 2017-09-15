7a371653ac92297d099d564abec8b1be64dfcaef
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2017-09-15 12:41:02 +0200

    StringHelper: Add Join for QVector of any numerical type

--- core/src/iAStringHelper.h
+++ core/src/iAStringHelper.h
@@ -20,12 +20,31 @@
 * ************************************************************************************/
 #pragma once
 
+#include "open_iA_Core_export.h"
+
+#include <QVector>
+
 class QString;
 class QStringList;
-
-#include "open_iA_Core_export.h"
 
 open_iA_Core_API QStringList SplitPossiblyQuotedString(QString const & additionalArguments);
 
 bool Str2Vec3D(QString const & str, double vec[3]);
 QString Vec3D2String(double* vec);
+
+
+template <typename T>
+QString Join(QVector<T> const & vec, QString const & joinStr)
+{
+	QString result;
+	bool first = true;
+	for (T elem : vec)
+	{
+		if (!first)
+			result += joinStr;
+		else
+			first = false;
+		result += QString::number(elem);
+	}
+	return result;
+}
