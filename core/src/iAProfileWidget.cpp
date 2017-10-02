3c9eb0194b88d0343a5ea08af834d5709c7877f4
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2017-10-02 14:17:12 +0200

    Profile Plot: adapt axis & plot to theme

--- core/src/iAProfileWidget.cpp
+++ core/src/iAProfileWidget.cpp
@@ -32,60 +32,46 @@
 
 iAProfileWidget::iAProfileWidget(QWidget *parent, vtkPolyData* profData, double rayLength, QString yCapt, QString xCapt) 
 	: iADiagramWidget (parent),
-	xPos(0)
+	xPos(0),
+	scalars(nullptr),
+	xCaption(xCapt),
+	yCaption(yCapt)
 {
 	min_intensity[0] = min_intensity[1] = min_intensity[2] = 0.0;
 	max_intensity[0] = max_intensity[1] = max_intensity[2] = 0.0;
-	scalars = 0;
-
 	initialize(profData, rayLength);
 	activeChild = parent->parentWidget();
-
-	yCaption = yCapt;
-	xCaption = xCapt;
 }
 
 
 void iAProfileWidget::initialize(vtkPolyData* profData, double rayLength)
 {
-	//initialise variables
 	profileData = profData;
-
 	numBin = profileData->GetNumberOfPoints();
 	rayLen = rayLength;
-
 	profileData->GetScalarRange(yDataRange);
 	scalars = profData->GetPointData()->GetScalars();
-
 	yHeight = yDataRange[1] - yDataRange[0];
-
-	//set attribut, so that the objects are deleted while
-	//this widget is closed
 	this->setAttribute(Qt::WA_DeleteOnClose);
 	this->setFocusPolicy(Qt::WheelFocus);
-
 	mode = NO_MODE;
-
 	draw = false;
 }
 
 
 void iAProfileWidget::paintEvent(QPaintEvent * )
 {
-	if (draw) this->drawProfilePlot();
-	//set the widget as the drawing device for the Qpainter painter
+	if (draw)
+		this->drawProfilePlot();
 	QPainter painter(this);
-	//use the painter to draw from Qimage image to the drawing device widget
 	painter.drawImage(QRectF(0, 0, width, height), image);
 }
 
 
 void iAProfileWidget::drawProfilePlot()
-{	
-	//initialise a painter
+{
 	QPainter painter(&image);
 	painter.setRenderHint(QPainter::Antialiasing);
-
 	drawBackground(painter);
 
 	//change the origin of the window to left bottom
@@ -94,14 +80,11 @@ void iAProfileWidget::drawProfilePlot()
 
 	drawHistogram(painter);
 
-	//draw functions
 	painter.scale(1, -1);
 	painter.setRenderHint(QPainter::Antialiasing, false);
 
 	drawAxes(painter);
 
-
-	//update the painter
 	update();
 	draw = false;
 }
@@ -117,9 +100,8 @@ void iAProfileWidget::redraw()
 void iAProfileWidget::selectBin(QMouseEvent *event)
 {
 	if (!scalars)
-	{
 		return;
-	}
+
 	xPos = clamp(0, ActiveWidth() - 1, event->x() - LeftMargin());
 	int nthBin = (int)((((xPos-translationX) * numBin) / ActiveWidth()) / xZoom);
 	double len = (((xPos-translationX) * rayLen) / ActiveWidth()) / xZoom;
@@ -141,30 +123,22 @@ void iAProfileWidget::selectBin(QMouseEvent *event)
 
 void iAProfileWidget::drawHistogram(QPainter &painter)
 {
+	if (!scalars)
+		return;
+
 	double binWidth = (double)(ActiveWidth()) / numBin *xZoom;
-
 	int intBinWidth = (int)binWidth;
-
 	if (intBinWidth < binWidth)
 		intBinWidth++;
-
-	//change the pen color to blue
-	painter.setPen(QColor(70,70,70,255));
-
-	if (scalars)
+	painter.setPen(QWidget::palette().color(QPalette::Text));
+	double scalingCoef = (double)(height-BottomMargin()-1) / yHeight *yZoom;
+	for ( int j = 0; j < numBin-1; j++ )
 	{
-		//draw the histogram using the painter on the image
-		double scalingCoef = (double)(height-BottomMargin()-1) / yHeight *yZoom;
-		for ( int j = 0; j < numBin-1; j++ ) 
-		{		 
-			double x1 = (int)(j * binWidth);
-			double y1 = (scalars->GetTuple1(j) - yDataRange[0]) * scalingCoef;
-			double x2 = (int)((j+1) * binWidth);
-			double y2 = (scalars->GetTuple1(j+1) - yDataRange[0]) * scalingCoef;
-
-			painter.drawLine(x1, y1, x2, y2);// draw line
-			//painter.fillRect(QRect(x1, 0, intBinWidth, y1), Qt::blue); // draw line 
-		}
+		double x1 = (int)(j * binWidth);
+		double y1 = (scalars->GetTuple1(j) - yDataRange[0]) * scalingCoef;
+		double x2 = (int)((j+1) * binWidth);
+		double y2 = (scalars->GetTuple1(j+1) - yDataRange[0]) * scalingCoef;
+		painter.drawLine(x1, y1, x2, y2);
 	}
 }
 
@@ -178,97 +152,63 @@ void iAProfileWidget::drawAxes(QPainter &painter)
 
 void iAProfileWidget::drawXAxis(QPainter &painter)
 {
-	painter.setPen(Qt::black);
-
-	//markings in x axis
+	painter.setPen(QWidget::palette().color(QPalette::Text));
 	int i = 0;
 	int stepNumber = (int)(10.0*xZoom);
 	double step = 1.0 / stepNumber;
 	QFontMetrics fm = painter.fontMetrics();
 	while (i <= stepNumber)
 	{
-		//calculate the nth bin located at a given pixel, actual formula is (i/100 * width) * (numBin / width)
 		double pos = step * i;
 		int nthBin = (int)(pos * rayLen);
-		//get the intensity value into a string
 		double value = nthBin;
-
-		QString text;
-		if (value < 1.0)
-			text = QString::number(value, 'g', 3);
-		else
-			text = QString::number((int)value, 10);
-
-		//calculate the x coordinate
+		QString text = (value < 1.0) ? QString::number(value, 'g', 3) : QString::number((int)value, 10);
 		int x = (int)(pos * ActiveWidth() * xZoom);
-		//draw a small indicator line
-		painter.drawLine(x, (int)(BottomMargin()*0.1), x, -1);
-
+		painter.drawLine(x, (int)(BottomMargin()*0.1), x, -1);		// draw a small indicator line
 		if (i <= 0)
-		{
-			painter.drawText(x, TEXT_Y, text ); //write the text right aligned to indicator line
-		}
+			painter.drawText(x, TEXT_Y, text );						// write the text right aligned to indicator line
 		else if(stepNumber == i)
-		{
-			painter.drawText(x - fm.width(text), TEXT_Y, text); //write the text left aligned to the indicator line
-		}
+			painter.drawText(x - fm.width(text), TEXT_Y, text);		// write the text left aligned to the indicator line
 		else
-		{
-			painter.drawText(x - 0.5*fm.width(text), TEXT_Y, text); //write the text centered to the indicator line
-		}
+			painter.drawText(x - 0.5*fm.width(text), TEXT_Y, text); // write the text centered to the indicator line
 		i++;
 	}
-	//draw the x axis
-	painter.drawLine(0, -1, (int)(ActiveWidth()*xZoom), -1);
-	//write the x axis label
-	painter.drawText( QPointF((int)(ActiveWidth() * 0.45 ), BottomMargin()-2), xCaption);
+	painter.drawLine(0, -1, (int)(ActiveWidth()*xZoom), -1);		// draw the x axis
+	painter.drawText(												// write the x axis label
+		QPointF((int)(ActiveWidth() * 0.45), BottomMargin() - 2),
+		xCaption);
 }
 
 
 void iAProfileWidget::drawYAxis(QPainter &painter)
 {
-	//change pen color to black
-	painter.setPen(Qt::black);
+	painter.setPen(QWidget::palette().color(QPalette::Text));
 	QFontMetrics fm = painter.fontMetrics();
 	int fontHeight = fm.height();
-
-	//markings in y axis
 	int i = 0;
 	int stepNumber = 5;
 	double step = 1.0 / (stepNumber*yZoom);
 	while (i <= stepNumber)
 	{
-		//calculate the nth bin located at a given pixel, actual formula is (i/100 * width) * (rayLength / width)
 		double pos = step * i;
-		//get the intensity value into a string
 		double yValue = pos * yHeight + yDataRange[0];
-
-		QString text;
-		if (yValue < 1.0)
-			text = QString::number(yValue, 'g', 3);
-		else
-			text = QString::number((int)yValue, 10);
-
-		//calculate the y coordinate
+		QString text = (yValue < 1.0) ? QString::number(yValue, 'g', 3) : QString::number((int)yValue, 10);
 		int y = -(int)(pos * ActiveHeight() * yZoom)-1;
-		//draw a small indicator line
-		painter.drawLine((int)(-LeftMargin()*0.1), y, 0, y);
+		painter.drawLine((int)(-LeftMargin()*0.1), y, 0, y);				// draw a small indicator line
 
 		if(i == stepNumber)
-			painter.drawText(TEXT_X-LeftMargin(), y+0.6*fontHeight, text); //write the text left aligned to the indicator line
+			painter.drawText(TEXT_X-LeftMargin(), y+0.6*fontHeight, text);	// write the text left aligned to the indicator line
 		else
-			painter.drawText(TEXT_X-LeftMargin(), y+0.25*fontHeight, text); //write the text centered to the indicator line
-
+			painter.drawText(TEXT_X-LeftMargin(), y+0.25*fontHeight, text);	// write the text centered to the indicator line
 		i++;
 	}
-	painter.drawLine(0, -1, 0, -(int)(ActiveHeight()*yZoom));
-	//write the y axis label
+	painter.drawLine(0, -1, 0, -(int)(ActiveHeight()*yZoom));				// draw the y axis
 	painter.save();
 	painter.rotate(-90);
-	painter.drawText(
+	painter.drawText(														// write the y axis label
 		QPointF(
-		ActiveHeight()*0.5 - 0.5*fm.width(yCaption),
-		-LeftMargin() + fontHeight - 5),
+			ActiveHeight()*0.5 - 0.5*fm.width(yCaption),
+			-LeftMargin() + fontHeight - 5),
 		yCaption);
 	painter.restore();
 }
