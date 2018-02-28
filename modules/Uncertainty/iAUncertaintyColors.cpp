19e81d130adf273814e850135682e825c7f667ef
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2018-02-28 17:28:28 +0100

    Uncertainty: Fix colors (uncertainty distribution -> violet)

--- modules/Uncertainty/iAUncertaintyColors.cpp
+++ modules/Uncertainty/iAUncertaintyColors.cpp
@@ -20,8 +20,13 @@
 * ************************************************************************************/
 #include "iAUncertaintyColors.h"
 
-QColor iAUncertaintyColors::ScatterPlotDots(27, 158, 119);
-QColor iAUncertaintyColors::Selection(230, 171, 2);
-QColor iAUncertaintyColors::MemberBar(217, 95,2);
-QColor iAUncertaintyColors::UncertaintyDistribution(166,118,29);
+// "qualitative" 7-class dark-2 from colorbrewer2.org:
+QColor iAUncertaintyColors::ScatterPlotDots(27, 158, 119);	// 1
+QColor iAUncertaintyColors::Selection(230, 171, 2);			// 6
+QColor iAUncertaintyColors::MemberBar(217, 95,2);			// 2
+QColor iAUncertaintyColors::UncertaintyDistribution(117,112,179);	// 3
+// (231,41,138); // 4
+// (102,166,30); // 5
+//(166,118,29);	// 7
+// except for this, this is "sequential" 9-class Blues color 9:
 QColor iAUncertaintyColors::LabelDistributionBase(8,48,107);
