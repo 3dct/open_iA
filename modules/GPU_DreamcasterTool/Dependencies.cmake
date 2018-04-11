b27d663d7e0d69b3b627c1c1885cf25a2b4446f9
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2018-04-11 14:07:02 +0200

    CMake: Remove empty module dependency clauses

--- modules/GPU_DreamcasterTool/Dependencies.cmake
+++ modules/GPU_DreamcasterTool/Dependencies.cmake
@@ -1,11 +1,3 @@
-# Modules which are used by the given module
-SET( DEPENDENCIES_MODULES
-)
-
-# Cmake defines which are used by the given module
-SET( DEPENDENCIES_CMAKE
-)
-
 # Libraries which are used by the module
 SET( DEPENDENCIES_LIBRARIES
 	Dreamcaster
