0314418998684c7361ce8cf9bbabff3804eabe1a
Bernhard Froehler <gitkraken@bfroehler.info>
2018-10-03 15:38:19 +0200

    FiberAnalytics: Remove load data timer

--- modules/FiberOptimizationExplorer/iAFiberCharData.cpp
+++ modules/FiberOptimizationExplorer/iAFiberCharData.cpp
@@ -27,7 +27,6 @@
 
 #include "charts/iASPLOMData.h"
 #include "iAConsole.h"
-#include "iAPerformanceHelper.h"
 #include "io/iAFileUtils.h" // for FindFiles
 
 #include <vtkFloatArray.h>
@@ -139,7 +138,6 @@ iAFiberResultsCollection::iAFiberResultsCollection():
 
 bool iAFiberResultsCollection::loadData(QString const & path, QString const & configName, iAProgress * progress)
 {
-	iATimeGuard perfLoad("Loading data...");
 	minFiberNumber = iARefDistCompute::MaxNumberOfCloseFibers;
 	QStringList filters;
 	filters << "*.csv";
