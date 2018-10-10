f1118f27ec65a8c756e3ba44f07a861cd930f1fa
Johannes Weissenböck <Johannes.Weissenboeck@fh-wels.at>
2018-10-10 15:13:40 +0200

    FXAA preprocessor statement added

--- core/src/iARenderer.cpp
+++ core/src/iARenderer.cpp
@@ -854,7 +854,11 @@ void iARenderer::setSlicePlane(int planeID, double originX, double originY, doub
 
 void iARenderer::ApplySettings(iARenderSettings & settings)
 {
+#if (VTK_MAJOR_VERSION > 7 || (VTK_MAJOR_VERSION == 7 && VTK_MINOR_VERSION > 0))
 	ren->SetUseFXAA(settings.UseFXAA);
+#else
+	DEBUG_LOG("FXAA Anti-Aliasing is not support with your VTK version");
+#endif
 	cam->SetParallelProjection(settings.ParallelProjection);
 	QColor bgTop(settings.BackgroundTop);
 	QColor bgBottom(settings.BackgroundBottom);
