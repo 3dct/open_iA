c8744c8832a24d9f43f9a0482de2d1410314cd7e
Bernhard Froehler <gitkraken@bfroehler.info>
2018-10-12 16:26:31 +0200

    FeatureScout: Fix distribution csv/histogram

--- modules/FeatureScout/dlg_FeatureScout.cpp
+++ modules/FeatureScout/dlg_FeatureScout.cpp
@@ -1755,11 +1755,19 @@ void dlg_FeatureScout::CsvDVSaveButton()
 	}
 
 	dlg_commoninput dlg( this, "DistributionViewCSVSaveDialog", inList, inPara, NULL );
-	if (dlg.exec() != QDialog::Accepted || (dlg.getCheckValue(0) != 2 && dlg.getCheckValue(1) != 2))
+	if (dlg.exec() != QDialog::Accepted)
 		return;
 
+	bool saveFile = dlg.getCheckValue(0) == 2;
+	bool showHistogram = dlg.getCheckValue(1) == 2;
+	if (!saveFile && !showHistogram)
+	{
+		QMessageBox::warning(this, "FeatureScout", "Please check either 'Save file' or 'Show histogram' (or both).");
+		return;
+	}
+
 	QString filename;
-	if ( dlg.getCheckValue(0) == 2 )
+	if (saveFile)
 	{
 		filename = QFileDialog::getSaveFileName( this, tr( "Save characteristic distributions" ), m_sourcePath, tr( "CSV Files (*.csv *.CSV)" ) );
 		if ( filename.isEmpty() )
@@ -1779,14 +1787,14 @@ void dlg_FeatureScout::CsvDVSaveButton()
 		double range[2] = { 0.0, 0.0 };
 		vtkDataArray *length = vtkDataArray::SafeDownCast(
 			this->tableList[this->activeClassItem->index().row()]->GetColumn( rowList.at( row ) ) );
-		range[0] = dlg.getDblValue(4 * row + 3);
-		range[1] = dlg.getDblValue(4 * row + 4);
+		range[0] = dlg.getDblValue(3 * row + 2);
+		range[1] = dlg.getDblValue(3 * row + 3);
 		//length->GetRange(range);
 
 		if ( range[0] == range[1] )
 			range[1] = range[0] + 1.0;
 
-		int numberOfBins = dlg.getDblValue(4 * row + 5);
+		int numberOfBins = dlg.getDblValue(3 * row + 4);
 		//int numberOfBins = dlg.getDblValue(row+2);
 		//double inc = (range[1] - range[0]) / (numberOfBins) * 1.001; //test
 		double inc = ( range[1] - range[0] ) / ( numberOfBins );
@@ -1854,7 +1862,7 @@ void dlg_FeatureScout::CsvDVSaveButton()
 		disTable->AddColumn( populations.GetPointer() );
 
 		//Writes csv file
-		if ( dlg.getCheckValue(0) == 2 )
+		if ( saveFile )
 		{
 			ofstream file( filename.toStdString().c_str(), std::ios::app );
 			if ( file.is_open() )
@@ -1892,7 +1900,7 @@ void dlg_FeatureScout::CsvDVSaveButton()
 		}
 
 		//Creates chart for each selected characteristic
-		if ( dlg.getCheckValue(1) == 2 )
+		if ( showHistogram )
 		{
 			vtkChartXY* chart = vtkChartXY::SafeDownCast( distributionChartMatrix
 															->GetChart( vtkVector2i( row % ( rowList.count() < 3 ? rowList.count() % 3 : 3 ), row / 3 ) ) );
@@ -1910,7 +1918,7 @@ void dlg_FeatureScout::CsvDVSaveButton()
 	}
 
 	//Renders the distributionMatrix in a new dockWidget
-	if ( dlg.getCheckValue(1) == 2 )
+	if ( showHistogram )
 	{
 		if ( !iovDV )
 		{
