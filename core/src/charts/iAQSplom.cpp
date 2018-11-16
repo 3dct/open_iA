f31f7080882d0bdc97ea71d173b059f90e7fb928
Bernhard Froehler <gitkraken@bfroehler.info>
2018-11-16 14:05:18 +0100

    Core: Fix SPLOM points update (Potential buffer::bind failed error)

--- core/src/charts/iAQSplom.cpp
+++ core/src/charts/iAQSplom.cpp
@@ -1396,6 +1396,8 @@ void iAQSplom::setParameterToColorCode(int paramIndex)
 
 void iAQSplom::updateLookupTable()
 {
+	if (m_splomData->numParams() == 0)
+		return;
 	double lutRange[2] = { m_settingsDlg->sbMin->value(), m_settingsDlg->sbMax->value() };
 	double alpha = static_cast<double>(m_settingsDlg->slPointOpacity->value()) / m_settingsDlg->slPointOpacity->maximum();
 	settings.pointColor.setAlpha(alpha*255);
