57a334a489ee51a6d322d7da36e5c859fd66a2de
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2019-03-27 13:29:04 +0100

    CTest/CMake 3.14 fix - use CTEST_SUBMIT_URL instead of CTEST_DROP_*

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,11 +5,14 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-
 set(CTEST_PROJECT_NAME "openiA")
 set(CTEST_NIGHTLY_START_TIME "00:01:00 CEST")
 
-set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "git.3dct.at")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=openiA")
+IF (CMAKE_MAJOR_VERSION GREATER 3 OR (CMAKE_MAJOR_VERSION EQUAL 3 AND CMAKE_MINOR_VERSION GREATER 13))
+	set(CTEST_SUBMIT_URL "https://git.3dct.at/CDash/submit.php?project=${CTEST_PROJECT_NAME}")
+ELSE()
+	set(CTEST_DROP_METHOD "https")
+	set(CTEST_DROP_SITE "git.3dct.at")
+	set(CTEST_DROP_LOCATION "/CDash/submit.php?project=${CTEST_PROJECT_NAME}")
+ENDIF()
 set(CTEST_DROP_SITE_CDASH TRUE)
