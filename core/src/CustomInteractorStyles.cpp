e481fb1ebf888cccc28b95cdafc771bce3abca25
MSchiwarth <marcel.schiwarth@fh-wels.at>
2019-04-01 11:13:22 +0200

    cleanup

--- core/src/CustomInteractorStyles.cpp
+++ core/src/CustomInteractorStyles.cpp
@@ -95,55 +95,10 @@ void iACustomInterActorStyleTrackBall::OnMouseMove()
 	assert(this->m_volumeRenderer && "prop 3D slicer null");
 	assert(this->m_propSlicer1.prop && "prop Slicer 1 null");
 	assert(this->m_propSlicer2.prop && "prop Slicer 2 null");
-
-
-
+	   
 	updateSlicer();
-
-	
-	
-
 }
 
-//	int x = this->Interactor->GetEventPosition()[0];
-//	int y = this->Interactor->GetEventPosition()[1];
-//
-//	this->FindPokedRenderer(x, y);
-//	this->Interactor->GetPicker()->Pick(x, y, 0, this->GetCurrentRenderer());
-//	this->FindPickedActor(x, y);
-//	if (this->CurrentRenderer == nullptr || this->m_PropCurrentSlicer.prop == nullptr
-//		|| this->m_propSlicer1.prop == nullptr || this->m_propSlicer2.prop == nullptr)
-//	{
-//		DEBUG_LOG("Either renderer or props are null");
-//		return;
-//	}
-//
-//	double picked[3];
-//	this->Interactor->GetPicker()->GetPickPosition(picked);
-//	DEBUG_LOG(QString("Picked 1% \t %2 \t %3").arg(picked[0]).arg(picked[1]).arg(picked[2]));
-//
-//	//connect the components; 
-//	printProbOrientation();
-//	printPropPosistion();
-//	printProbOrigin();
-//
-//
-//
-//
-//	assert(this->m_volumeRenderer && "prop 3D slicer null");
-//	assert(this->m_propSlicer1.prop && "prop Slicer 1 null");
-//	assert(this->m_propSlicer2.prop && "prop Slicer 2 null");
-//
-//
-//	updateSlicer();
-//
-//
-//	if (!this->Interactor->GetShiftKey()){
-//		return;
-//		vtkInteractorStyleTrackballActor::OnMouseMove();
-//	}
-
-	// Call parent to handle all other states and perform additional work
 
 	
 
