22e23cfef3889610dbe55f3bd4b77a351caa440a
MSchiwarth <marcel.schiwarth@fh-wels.at>
2019-05-16 15:34:46 +0200

    helper class for perform the transformation

---
+++ core/src/TransformHelper.cpp
@@ -0,0 +1,12 @@
+#include "TransformHelper.h"
+
+
+
+TransformHelper::TransformHelper()
+{
+}
+
+
+TransformHelper::~TransformHelper()
+{
+}
---
+++ core/src/TransformHelper.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace TransformHelper {
+
+	enum rotatationMode {
+		x, y; z; 
+
+	};
+
+}
+
+//should perform all transformation
+
+class TransformHelper
+{
+public:
+	TransformHelper();
+	~TransformHelper();
+};
+
