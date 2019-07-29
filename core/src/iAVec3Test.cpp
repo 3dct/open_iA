2477b34ce51d399b3ad4ec6ba73db25209d7f20a
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2019-07-29 17:37:51 +0200

    Add iAVec3Test

---
+++ core/src/iAVec3Test.cpp
@@ -0,0 +1,30 @@
+/*************************************  open_iA  ************************************ *
+* **********   A tool for visual analysis and processing of 3D CT images   ********** *
+* *********************************************************************************** *
+* Copyright (C) 2016-2019  C. Heinzl, M. Reiter, A. Reh, W. Li, M. Arikan, Ar. &  Al. *
+*                          Amirkhanov, J. Weissenböck, B. Fröhler, M. Schiwarth       *
+* *********************************************************************************** *
+* This program is free software: you can redistribute it and/or modify it under the   *
+* terms of the GNU General Public License as published by the Free Software           *
+* Foundation, either version 3 of the License, or (at your option) any later version. *
+*                                                                                     *
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY     *
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A     *
+* PARTICULAR PURPOSE.  See the GNU General Public License for more details.           *
+*                                                                                     *
+* You should have received a copy of the GNU General Public License along with this   *
+* program.  If not, see http://www.gnu.org/licenses/                                  *
+* *********************************************************************************** *
+* Contact: FH OÖ Forschungs & Entwicklungs GmbH, Campus Wels, CT-Gruppe,              *
+*          Stelzhamerstraße 23, 4600 Wels / Austria, Email: c.heinzl@fh-wels.at       *
+* ************************************************************************************/
+
+#include "iASimpleTester.h"
+#include "iAvec3.h"
+
+BEGIN_TEST
+	iAVec3i vec1(0, 1, 2);
+	iAVec3i vec2(2, 3, 4);
+	iAVec3i resVec = vec1 + vec2;
+	TestAssert(resVec[0] == 2 && resVec[1] == 4 && resVec[2] == 6);
+END_TEST
