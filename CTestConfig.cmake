0d89cfde24397e142ec70b52143ac720ac6b1938
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2019-08-26 15:09:52 +0200

    CTest: Shorten config file comments

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,10 +1,6 @@
-## This file should be placed in the root directory of your project.
-## Then modify the CMakeLists.txt file in the root directory of your
-## project to incorporate the testing dashboard.
+# Required to use CTest / CDash
+# enabled via ENABLE_TESTING() / INCLUDE(CTest) in CMakeLists.txt (in cmake/Modules/Common.cmake in our case)
 
-## # The following are required to uses Dart and the Cdash dashboard
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
 set(CTEST_PROJECT_NAME "openiA")
 set(CTEST_NIGHTLY_START_TIME "00:01:00 CEST")
 
