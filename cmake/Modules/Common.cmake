424336e498950c599b13525c1fca2e8a5dd28a0d
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2019-11-27 12:47:47 +0100

    CMake: Fix win32 build for CMake < 3.14 (get_filename_component NAME_WLE)

--- cmake/Modules/Common.cmake
+++ cmake/Modules/Common.cmake
@@ -345,9 +345,12 @@ FIND_PACKAGE(OpenCL)
 IF (OPENCL_FOUND)
 	MESSAGE(STATUS "OpenCL: include=${OPENCL_INCLUDE_DIRS}, libraries=${OPENCL_LIBRARIES}.")
 	IF (WIN32)
-		# Find path of OpenCL.dll to include in release:
+				# Find path of OpenCL.dll to include in release:
 		get_filename_component(OPENCL_LIB_DIR "${OPENCL_LIBRARIES}" DIRECTORY)
-		get_filename_component(OPENCL_LIB_BASENAME "${OPENCL_LIBRARIES}" NAME_WLE)
+		message(STATUS "get_filename_component(OPENCL_LIB_BASENAME ${OPENCL_LIBRARIES} NAME_WLE)")
+		# NAME_WLE only introduced in CMake 3.14
+		# get_filename_component(OPENCL_LIB_BASENAME "${OPENCL_LIBRARIES}" NAME_WLE)
+		get_filename_component(OPENCL_LIB_BASENAME "${OPENCL_LIBRARIES}" NAME_WE)
 		STRING(REGEX REPLACE "lib" "bin" OPENCL_DLL_DIR "${OPENCL_LIB_DIR}")
 		IF (NOT EXISTS "${OPENCL_DLL_DIR}/${OPENCL_LIB_BASENAME}.dll")
 			MESSAGE(STATUS "Directory containing ${OPENCL_LIB_BASENAME}.dll was not found. You can continue building, but the program might not run (or it might fail to run when installed/cpacked).")
