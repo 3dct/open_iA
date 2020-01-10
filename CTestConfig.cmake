cbd55c50ca3a957bfad0052e32abaed9b702b9a4
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2020-01-10 09:55:42 +0100

    CMake: Remove ineffective CDash warning limit setting

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,7 @@
 # Required to use CTest / CDash
 # enabled via ENABLE_TESTING() / INCLUDE(CTest) in CMakeLists.txt (in cmake/Modules/Common.cmake in our case)
 set(CTEST_PROJECT_NAME "openiA")
-set(DART_TESTING_TIMEOUT 120)
+set(CTEST_NIGHTLY_START_TIME "18:00:00 CEST")
 
 IF (CMAKE_MAJOR_VERSION GREATER 3 OR (CMAKE_MAJOR_VERSION EQUAL 3 AND CMAKE_MINOR_VERSION GREATER 13))
 	set(CTEST_SUBMIT_URL "https://git.3dct.at/CDash/submit.php?project=${CTEST_PROJECT_NAME}")
@@ -11,4 +11,3 @@ ELSE()
 	set(CTEST_DROP_LOCATION "/CDash/submit.php?project=${CTEST_PROJECT_NAME}")
 ENDIF()
 set(CTEST_DROP_SITE_CDASH TRUE)
-set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1000)
