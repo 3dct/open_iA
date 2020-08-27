fdecff531a679f3596c94aa74bd39174b3d59f8b
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2020-08-27 13:50:15 +0200

    FuzzyFeatureTracking: Fix nrOfOccurences

--- modules/FuzzyFeatureTracking/iAFeatureTracking.cpp
+++ modules/FuzzyFeatureTracking/iAFeatureTracking.cpp
@@ -133,10 +133,9 @@ void iAFeatureTracking::sortCorrespondencesByOverlap(std::vector<iAFeatureTracki
 int nrOfOccurences(std::vector<int>& v, int occurence)
 {
 	int result = 0;
-	// this loop probably does not do what it's supposed to? only compares index of v, not its content!
-	for (int i = 0; i < v.size(); ++i)
+	for (size_t i = 0; i < v.size(); ++i)
 	{
-		if (i == occurence)	// v[i] == occurrence ?
+		if (v[i] == occurence)
 		{
 			++result;
 		}
