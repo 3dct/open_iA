4914031ba4b35dff8bf6c3d660e262e1db27e208
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2020-09-01 16:05:35 +0200

    AI: Add option for switching between DirectML and CUDA backend

--- modules/AI/enabled.cmake
+++ modules/AI/enabled.cmake
@@ -1,3 +1,4 @@
-if (UNIX)
+option (openiA_ONNX_CUDA "Whether to use CUDA for running ONNX. If disabled, DirectML will be used (on Windows)." OFF)
+if (UNIX OR openiA_ONNX_CUDA)
 	TARGET_COMPILE_DEFINITIONS(AI PRIVATE ONNX_CUDA)
 endif()
