2729cb44cec50eb9ccb1fbc57bdc3019c2c5b7fa
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2021-01-12 20:12:58 +0100

    CMake: Adapt RPath change script to also fix Qt plugins and open_iA plugins

--- cmake/ChangeRPath.sh
+++ cmake/ChangeRPath.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # patch all shared objects to have an RPATH of '$ORIGIN':
-for file in *.so*
+for lib in *.so*
 do
-	# echo "Patching runpath in $file"
-	if [ ! -f "$file" ]
+	# echo "Patching runpath in $lib"
+	if [ ! -f "$lib" ]
 	then
-		echo "ChangeRPath.sh: $file not found!"
+		echo "ChangeRPath.sh: $lib not found!"
 	else
-		echo "Patching RPATH in $file"
-		patchelf --set-rpath '$ORIGIN' $file
+		patchelf --set-rpath '$ORIGIN' $lib
 	fi
+done
+
+for lib in plugins/*.so
+do
+	patchelf --set-rpath '$ORIGIN/..' ${lib}
+	# Remove any plugin libraries copied directly to root folder because of dependencies:
+	rootpluglib=$(basename $lib)
+	rm -f $rootpluglib
 done
 
 declare -a libarray=("iconengines/libqsvgicon.so" "imageformats/libqsvg.so" "platforms/libqxcb.so" "xcbglintegrations/libqxcb-glx-integration.so" "xcbglintegrations/libqxcb-egl-integration.so")
@@ -21,7 +28,7 @@ do
 	then
 		echo "ChangeRPath.sh: ${lib} not found!"
 	else
-		echo "Patching RPATH in $lib"
+		#echo "Patching RPATH in $lib"
 		patchelf --set-rpath '$ORIGIN/..' ${lib}
 	fi
 done
