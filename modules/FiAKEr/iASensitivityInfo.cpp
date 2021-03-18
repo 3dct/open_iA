ede36ecfc56049c5d0e42cadaa4e20f537895f93
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2021-03-18 08:56:19 +0100

    Sensitivity: Do characteristics range check also when loading from project

--- modules/FiAKEr/iASensitivityInfo.cpp
+++ modules/FiAKEr/iASensitivityInfo.cpp
@@ -382,22 +382,23 @@ QSharedPointer<iASensitivityInfo> iASensitivityInfo::create(QMainWindow* child,
 		}
 		sensitivity->m_charSelected = dlg.selectedCharacteristics();
 		sensitivity->m_charDiffMeasure = dlg.selectedDiffMeasures();
-		for (int j = sensitivity->m_charSelected.size() - 1; j >= 0; --j)
-		{
-			int charIdx = sensitivity->m_charSelected[j];
-			// make sure of all histograms for the same characteristic have the same range
-			if (data->spmData->paramRange(charIdx)[0] == data->spmData->paramRange(charIdx)[1])
-			{
-				LOG(lvlInfo, QString("Characteristic %1 does not vary, excluding from analysis!")
-					.arg(data->spmData->parameterName(charIdx)));
-				sensitivity->m_charSelected.remove(j);
-			}
-		}
 	}
 	else
 	{
 		sensitivity->m_charSelected = charSelected;
 		sensitivity->m_charDiffMeasure = charDiffMeasure;
+	}
+	for (int j = sensitivity->m_charSelected.size() - 1; j >= 0; --j)
+	{
+		int charIdx = sensitivity->m_charSelected[j];
+		// make sure of all histograms for the same characteristic have the same range
+		if (data->spmData->paramRange(charIdx)[0] == data->spmData->paramRange(charIdx)[1])
+		{
+			LOG(lvlInfo,
+				QString("Characteristic %1 does not vary, excluding from analysis!")
+					.arg(data->spmData->parameterName(charIdx)));
+			sensitivity->m_charSelected.remove(j);
+		}
 	}
 	//sensitivity->dissimMeasure = dlg.selectedMeasures();
 	sensitivity->m_histogramBins = histogramBins;
