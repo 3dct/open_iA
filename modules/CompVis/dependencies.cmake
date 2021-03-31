4b2d4654c1a1792b10f5b02bc605084ff0fe421a
Anja Heim <anja.heim@fh-wels.at>
2021-03-31 11:32:37 +0200

    updated dependencies.cmake to work with msvc2019_64

--- modules/CompVis/dependencies.cmake
+++ modules/CompVis/dependencies.cmake
@@ -8,4 +8,9 @@ SET( DEPENDENCIES_VTK_MODULES
 	RenderingContext2D    # for vtkContextActor, vtkContextScene, vtkContextTransform
 	RenderingContext${VTK_RENDERING_BACKEND}    # for implementation of RenderingContext2D (?)
 	ViewsContext2D        # for vtkContextView, vtkContextInteractorStyle
+	InfovisCore
+	InfovisLayout
+	ViewsInfovis
+	FiltersStatistics
+	FiltersExtraction
 )
