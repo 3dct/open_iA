aecb5b31d287562823dc322ea0f0e87835d1c68a
Alexander Gall <alexander.gall@fh-wels.at>
2021-10-25 12:03:04 +0200

    XVRA: added dependency for boost

--- modules/XVRA/Dependencies.cmake
+++ modules/XVRA/Dependencies.cmake
@@ -6,3 +6,13 @@ set(DEPENDENCIES_MODULES
 	FeatureScout           
 	VR
 )
+
+# Check whether boost (from astra) has histogram.hpp (only available in boost >= 1.70)
+if (NOT BOOST_INCLUDE_DIR OR NOT EXISTS "${BOOST_INCLUDE_DIR}/boost/histogram.hpp")
+	message(STATUS "Boost with histogram.hpp not found (specify via BOOST_INCLUDE_DIR)!")
+	set(DEPENDENCIES_CMAKE BOOST_HISTOGRAM_HPP_FOUND)
+endif()
+
+set(DEPENDENCIES_INCLUDE_DIRS
+	${BOOST_INCLUDE_DIR}
+)
