625a73d38117b77c9508bf66229857d9e25b4a06
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2022-07-04 16:26:43 +0200

    base: add join for std::string (iAStringHelper)

--- libs/base/iAStringHelper.cpp
+++ libs/base/iAStringHelper.cpp
@@ -125,6 +125,17 @@ QString dblToStringWithUnits(double value, double switchFactor)
 	return QString::number(value, 'g', 3);
 }
 
+
+// source: https://stackoverflow.com/a/12155571
+iAbase_API std::string joinStdString(std::vector<std::string>& vec, std::string const& joinStr)
+{
+	return vec.empty() ? "" :           // leave early if there are no items in the list
+		std::accumulate(                // otherwise, accumulate
+			++vec.begin(), vec.end(),   // the range 2nd to after-last
+			*vec.begin(),               // and start accumulating with the first item
+			[joinStr](auto& a, auto& b) { return a + joinStr + b; });
+}
+
 QString joinQVariantAsString(QVector<QVariant> const& vec, QString const& joinStr)
 {
 	QString result;
