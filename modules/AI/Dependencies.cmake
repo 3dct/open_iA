d444429418556e24045e66a25797f6189c398565
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2022-08-17 13:37:56 +0200

    ONNX: Fix DirectML include/lib directory finding

--- modules/AI/Dependencies.cmake
+++ modules/AI/Dependencies.cmake
@@ -7,10 +7,12 @@ set(ONNX_RUNTIME_DIR
 find_library(ONNX_RUNTIME_LIBRARIES onnxruntime
 	PATHS
 		${ONNX_RUNTIME_DIR}/lib
+		${ONNX_RUNTIME_DIR}/runtimes/win-x64/native
 )
 find_path(ONNX_RUNTIME_INCLUDE_DIR onnxruntime_cxx_api.h
 	PATHS
 		${ONNX_RUNTIME_DIR}/include
+		${ONNX_RUNTIME_DIR}/build/native/include
 )
 set(DEPENDENCIES_LIBRARIES
 	${ONNX_RUNTIME_LIBRARIES}
