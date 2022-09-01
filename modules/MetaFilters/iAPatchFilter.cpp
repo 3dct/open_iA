cb1347586602e01fdd86b8d5c4b8f65a8b4d0b22
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2022-09-01 08:29:16 +0200

    MetaFilters: Fix iAImageData::create call

--- modules/MetaFilters/iAPatchFilter.cpp
+++ modules/MetaFilters/iAPatchFilter.cpp
@@ -196,7 +196,7 @@ namespace
 						{
 							auto itkExtractImg = extractImage(dynamic_cast<iAImageData*>(inputImages[i].get())->itkImage(), extractIndex, extractSize);
 							// maybe modify original filename to reflect that only a patch of it is passed on?
-							filter->addInput(iAImageData::create("", itkExtractImg));
+							filter->addInput(iAImageData::create(itkExtractImg, ""));
 						}
 						// run filter on inputs:
 						filter->run(filterParams);
