e88ec7f99fc8348731d95af30c606d7d4704d2ae
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2022-10-05 12:04:08 +0200

    Call npm install/build during Remote module build

--- modules/Remote/enabled.cmake
+++ modules/Remote/enabled.cmake
@@ -1,7 +1,15 @@
 if (Qt${QT_VERSION_MAJOR}HttpServer_FOUND)
+
 	target_compile_definitions(Remote PRIVATE QT_HTTPSERVER)
 
 	set(HTTP_INSTALL_SRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}/Remote/web/dist")
+	add_custom_target(RemoteClientCode
+		COMMAND npm run installbuild
+		WORKING_DIRECTORY ${HTTP_INSTALL_SRC_DIR})
+	if (openiA_USE_IDE_FOLDERS)
+		SET_PROPERTY(TARGET RemoteClientCode PROPERTY FOLDER "Modules")
+	endif()
+	add_dependencies(Remote RemoteClientCode)
 	foreach (cfg ${CMAKE_CONFIGURATION_TYPES})
 		string(TOUPPER "${cfg}" CFG)
 		set(HTTP_DST_SUBDIR "RemoteClient")
