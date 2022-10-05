3caceb041f7b40134d23f15cbfca60413e5bba89
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2022-10-05 13:01:04 +0200

    NDTFlix: Copy js files to bin folder after building

--- modules/Remote/enabled.cmake
+++ modules/Remote/enabled.cmake
@@ -1,23 +1,13 @@
 if (Qt${QT_VERSION_MAJOR}HttpServer_FOUND)
-
 	target_compile_definitions(Remote PRIVATE QT_HTTPSERVER)
-
 	set(HTTP_INSTALL_SRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}/Remote/web/dist")
-	add_custom_target(RemoteClientCode
+	add_custom_command(TARGET Remote PRE_BUILD
+		BYPRODUCTS ${HTTP_INSTALL_SRC_DIR}/main.js
 		COMMAND npm run installbuild
 		WORKING_DIRECTORY ${HTTP_INSTALL_SRC_DIR})
-	if (openiA_USE_IDE_FOLDERS)
-		SET_PROPERTY(TARGET RemoteClientCode PROPERTY FOLDER "Modules")
-	endif()
-	add_dependencies(Remote RemoteClientCode)
-	foreach (cfg ${CMAKE_CONFIGURATION_TYPES})
-		string(TOUPPER "${cfg}" CFG)
-		set(HTTP_DST_SUBDIR "RemoteClient")
-		set(HTTP_INSTALL_DST_DIR "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_${CFG}}/${HTTP_DST_SUBDIR}")
-		if (NOT EXISTS "${HTTP_INSTALL_DST_DIR}")
-			file(COPY "${HTTP_INSTALL_SRC_DIR}/" DESTINATION "${HTTP_INSTALL_DST_DIR}")
-		endif()
-	endforeach()
+	set(HTTP_DST_SUBDIR "RemoteClient")
+	add_custom_command(TARGET Remote POST_BUILD
+		COMMAND ${CMAKE_COMMAND} -E copy_directory ${HTTP_INSTALL_SRC_DIR}/ $<IF:$<CONFIG:Debug>,${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG},$<IF:$<CONFIG:Release>,${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE},$<IF:$<CONFIG:RelWithDebInfo>,${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO},${CMAKE_RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL}>>>/${HTTP_DST_SUBDIR})
 	install(DIRECTORY "${HTTP_INSTALL_SRC_DIR}/" DESTINATION ${HTTP_DST_SUBDIR})
 else()
 	message(WARNING "Qt HttpServer is not available! You can still run the Remote Render Server, but you will have to serve the 'RemoteClient' folder separately, e.g. via webpack!")
