b5f46ddc5d243d7ac57b2a2643b03d3ba0a58fad
Patrick Weinberger <Patrick.Weinberger@fh-Wels.at>
2022-11-11 14:29:07 +0100

    Add angular

---
+++ modules/Remote/web/src_angular/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {TableRowBindingExample} from './table-row-binding-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [TableRowBindingExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [TableRowBindingExample],
+})
+export class AppModule {}
