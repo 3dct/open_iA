a69f90cec0b3fb9338f48b4e31c3f575013619a0
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2023-02-02 15:33:32 +0100

    gui: bezier function icon

--- libs/charts/iAChartWithFunctionsWidget.cpp
+++ libs/charts/iAChartWithFunctionsWidget.cpp
@@ -329,8 +329,8 @@ void iAChartWithFunctionsWidget::addContextMenuEntries(QMenu* contextMenu)
 		contextMenu->addSeparator();
 	}
 	if (m_enableAdditionalFunctions)
-	{
-		contextMenu->addAction(QIcon(":/images/addBezier.png"), tr("Add bezier function"), this, &iAChartWithFunctionsWidget::addBezierFunction);
+	{   // TODO: adapt icon to current theme (bright/dark -> when dark, use _light.svg!)
+		contextMenu->addAction(QIcon(":/images/bezier.svg"), tr("Add bezier function"), this, &iAChartWithFunctionsWidget::addBezierFunction);
 		contextMenu->addAction(QIcon(":/images/gaussian.svg"), tr("Add gaussian function"), this, QOverload<>::of(&iAChartWithFunctionsWidget::addGaussianFunction));
 		contextMenu->addAction(QIcon(":/images/openFkt.png"), tr("Load functions"), this, &iAChartWithFunctionsWidget::loadFunctions);
 		contextMenu->addAction(QIcon(":/images/saveFkt.png"), tr("Save functions"), this, &iAChartWithFunctionsWidget::saveFunctions);
