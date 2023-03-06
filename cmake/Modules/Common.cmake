b28f1f041b3f5333b8547aa12fa9516c0032f64b
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2023-03-06 09:04:50 +0100

    Remove WebSockets from Qt components to enable build without Remote module

--- cmake/Modules/Common.cmake
+++ cmake/Modules/Common.cmake
@@ -325,7 +325,7 @@ find_package(QT NAMES Qt6 Qt5 COMPONENTS Widgets OpenGLWidgets REQUIRED)
 #	set(Qt${QT_VERSION_MAJOR}GuiTools_DIR ${QT_DIR}GuiTools CACHE PATH "" FORCE)
 #	set(Qt${QT_VERSION_MAJOR}WidgetsTools_DIR ${QT_DIR}WidgetsTools CACHE PATH "" FORCE)
 #endif
-find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Concurrent Gui OpenGL Svg WebSockets Widgets Xml REQUIRED)
+find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Concurrent Gui OpenGL Svg Widgets Xml REQUIRED)
 message(STATUS "Qt: ${QT_VERSION} in ${Qt${QT_VERSION_MAJOR}_DIR}")
 set(BUILD_INFO "${BUILD_INFO}    \"Qt	${QT_VERSION}\\n\"\n")
 if (QT_VERSION VERSION_LESS "5.15.0")
