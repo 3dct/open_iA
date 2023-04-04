db7423e9c0403871d76b3fadb2ac986c31c26f6b
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2023-04-04 14:33:47 +0200

    AI: Add search for and installation of DirectML.dll

--- modules/AI/enabled.cmake
+++ modules/AI/enabled.cmake
@@ -7,3 +7,17 @@ endif()
 if (openiA_ONNX_NEWNAMEFUNCTIONS)
 	target_compile_definitions(AI PRIVATE ONNX_NEWNAMEFUNCTIONS)
 endif()
+
+find_library(DIRECTML_LIBRARY DirectML
+	PATHS
+		${ONNX_RUNTIME_DIR}/lib
+		${ONNX_RUNTIME_DIR}/runtimes/win-x64/native
+)
+
+if (NOT openiA_ONNX_CUDA)
+	if (${DIRECTML_LIBRARY} STREQUAL "DIRECTML_LIBRARY-NOTFOUND")
+		message(WARNING "You did not specify a DirectML library to use! If you build a release, the AI segmentation will not work!")
+	else()
+		install(FILES ${DIRECTML_LIBRARY} DESTINATION .)
+	endif()
+endif()
