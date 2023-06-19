e2ab88a8a9f5e83fa6c21a935d68a194da2f4662
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2023-06-19 16:37:32 +0200

    io: Add .obj file reader/writer

---
+++ libs/io/iAOBJFileIO.cpp
@@ -0,0 +1,49 @@
+// Copyright 2016-2023, the open_iA contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+#include "iAOBJFileIO.h"
+
+#include "iAExceptionThrowingErrorObserver.h"
+#include "iAFileUtils.h"
+#include "iAProgress.h"
+
+#include <vtkOBJReader.h>
+#include <vtkOBJWriter.h>
+
+iAOBJFileIO::iAOBJFileIO() : iAFileIO(iADataSetType::Mesh, iADataSetType::Mesh)
+{
+}
+
+std::shared_ptr<iADataSet> iAOBJFileIO::loadData(QString const& fileName, QVariantMap const& params, iAProgress const& progress)
+{
+	Q_UNUSED(params);
+	vtkNew<vtkOBJReader> reader;
+	progress.observe(reader);
+	reader->AddObserver(vtkCommand::ErrorEvent, iAExceptionThrowingErrorObserver::New());
+	reader->SetFileName(getLocalEncodingFileName(fileName).c_str());
+	vtkNew<vtkPolyData> polyData;
+	reader->SetOutput(polyData);
+	reader->Update();
+	return { std::make_shared<iAPolyData>(polyData) };
+}
+
+void iAOBJFileIO::saveData(QString const& fileName, std::shared_ptr<iADataSet> dataSet, QVariantMap const& paramValues, iAProgress const& progress)
+{
+	assert(dataSet->type() == iADataSetType::Mesh);
+	Q_UNUSED(paramValues);
+	vtkNew<vtkOBJWriter> writer;
+	progress.observe(writer);
+	writer->AddObserver(vtkCommand::ErrorEvent, iAExceptionThrowingErrorObserver::New());
+	writer->SetFileName(getLocalEncodingFileName(fileName).c_str());
+	writer->SetInputData(dynamic_cast<iAPolyData*>(dataSet.get())->poly());
+	writer->Write();
+}
+
+QString iAOBJFileIO::name() const
+{
+	return "WaveFront OBJ file";
+}
+
+QStringList iAOBJFileIO::extensions() const
+{
+	return QStringList{ "obj" };
+}
---
+++ libs/io/iAOBJFileIO.h
@@ -0,0 +1,18 @@
+// Copyright 2016-2023, the open_iA contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+#include "iAAutoRegistration.h"
+#include "iAFileIO.h"
+#include "iAFileTypeRegistry.h"
+
+//! Support for loading/saving OBJ files for the file I/O framework of open_iA.
+class iAOBJFileIO : public iAFileIO, private iAAutoRegistration<iAFileIO, iAOBJFileIO, iAFileTypeRegistry>
+{
+public:
+	iAOBJFileIO();
+	std::shared_ptr<iADataSet> loadData(QString const& fileName, QVariantMap const& paramValues, iAProgress const& progress) override;
+	void saveData(QString const& fileName, std::shared_ptr<iADataSet> dataSet, QVariantMap const& paramValues, iAProgress const& progress) override;
+	QString name() const override;
+	QStringList extensions() const override;
+};
