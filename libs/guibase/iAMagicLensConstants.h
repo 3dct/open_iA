5c77f72887053363896c664e3374c7033b8ff8f7
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2023-10-09 11:13:05 +0200

    guibase: Add missing iAMagicLensConstants header file

---
+++ libs/guibase/iAMagicLensConstants.h
@@ -0,0 +1,9 @@
+// Copyright 2016-2023, the open_iA contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+// for both 2D and 3D magic lenses
+const int DefaultMagicLensSize = 120;
+const int MinimumMagicLensSize = 40;
+const int MaximumMagicLensSize = 8192;
+const int DefaultMagicLensFrameWidth = 3;
