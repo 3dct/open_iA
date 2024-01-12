49ad3a4605921c0e8138160b723df19dd8204530
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2024-01-12 10:00:37 +0100

    gui: Fix setting 3D magic lens size / frame width

--- libs/gui/mdichild.cpp
+++ libs/gui/mdichild.cpp
@@ -166,6 +166,10 @@ void MdiChild::connectSignalsToSlots()
 		{
 			m_dwSlicer[i]->showBorder(m_renderer->isShowSlicePlanes());
 		}
+		auto s = m_renderer->settings()[iARendererImpl::MagicLensSize].toInt();
+		auto fw = m_renderer->settings()[iARendererImpl::MagicLensFrameWidth].toInt();
+		m_dwRenderer->vtkWidgetRC->setLensSize(s, s);
+		m_dwRenderer->vtkWidgetRC->setFrameWidth(fw);
 	};
 	connect(m_renderer, &iARendererImpl::settingsChanged, this, adaptSlicerBorders);
 	adaptSlicerBorders();
