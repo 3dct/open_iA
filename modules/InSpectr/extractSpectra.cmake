61e0f5087958f8de9112c70e3c9217a8290962d3
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2024-04-21 00:13:34 +0200

    InSpectr: CMake - split make_directory into separate function call

--- modules/InSpectr/extractSpectra.cmake
+++ modules/InSpectr/extractSpectra.cmake
@@ -1,10 +1,8 @@
 if (NOT EXISTS "${TARGET_DIR}" OR "${SPECTRA_ARCHIVE}" IS_NEWER_THAN "${TARGET_DIR}")
 	message("Extracting ${SPECTRA_ARCHIVE} into target directory ${TARGET_DIR}.")
-	execute_process(
-		COMMAND "${CMAKE_COMMAND}" -E make_directory "${TARGET_DIR}"
-		COMMAND "${CMAKE_COMMAND}" -E chdir "${TARGET_DIR}" "${CMAKE_COMMAND}" -E tar "xf" "${SPECTRA_ARCHIVE}"
-		RESULT_VARIABLE res
-		OUTPUT_VARIABLE out)
+	file(MAKE_DIRECTORY "${TARGET_DIR}")
+	execute_process(COMMAND "${CMAKE_COMMAND}" -E tar "xf" "${SPECTRA_ARCHIVE}" WORKING_DIRECTORY "${TARGET_DIR}"
+		RESULT_VARIABLE res	OUTPUT_VARIABLE out)
 	if (NOT "${res}" EQUAL "0")
 		message(SEND_ERROR "  Extracting ${SPECTRA_ARCHIVE} to ${TARGET_DIR} FAILED! exit code=${res}, output: ${out}")
 	endif()
