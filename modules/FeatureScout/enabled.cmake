a7a4e280c532236acc500a5f8a43b295650fe19a
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2024-07-30 16:04:00 +0200

    test: image output xml only on successful test, name screenshots after test name, fix screenshot filename reference

--- modules/FeatureScout/enabled.cmake
+++ modules/FeatureScout/enabled.cmake
@@ -1,5 +1,5 @@
 if (openiA_TESTING_ENABLED)
-	# TODO: copied from gui subfolde; unify?
+	# TODO: quite some overlap between here and gui subfolder; unify?
 	if (MSVC)
 		# use the created .bat which makes sure all required dll's are in the path:
 		set(GUITestBinary "${CMAKE_BINARY_DIR}/x64/open_iA_gui_Release.bat")
@@ -14,17 +14,17 @@ if (openiA_TESTING_ENABLED)
 	set(TEST_TEMPLATE_SCRIPT_FILE "${CMAKE_SOURCE_DIR}/test/run_test_with_img_output.cmake.in")
 
 	# FeatureScoutNoAutoIDTest
-	set(TEST_OUTPUT_FILE ${TEST_OUTPUT_DIR}/5-ROI-FeatureScout-LabelVis-NoAutoID.png)
 	set(TEST_NAME "FeatureScoutNoAutoIDTest")
-	set(TEST_COMMAND ${GUITestBinary} ${CMAKE_SOURCE_DIR}/test/data/fibers/5-ROI-FeatureScout-LabelVis-NoAutoID.iaproj --quit 250 --screenshot ${FeatureScoutNoAutoIDTest_OUTPUT})
+	set(TEST_OUTPUT_FILE ${TEST_OUTPUT_DIR}/${TEST_NAME}.png)
+	set(TEST_COMMAND ${GUITestBinary} ${CMAKE_SOURCE_DIR}/test/data/fibers/5-ROI-FeatureScout-LabelVis-NoAutoID.iaproj --quit 250 --screenshot ${TEST_OUTPUT_FILE})
 	set(TEST_SCRIPT_NAME "${CMAKE_BINARY_DIR}/${TEST_NAME}.cmake")
 	configure_file(${TEST_TEMPLATE_SCRIPT_FILE} ${TEST_SCRIPT_NAME} @ONLY)
 	add_test(NAME ${TEST_NAME} COMMAND ${CMAKE_COMMAND} -P ${TEST_SCRIPT_NAME})
 
 	# FeatureScoutWithAutoIDTest
-	set(TEST_OUTPUT_FILE ${TEST_OUTPUT_DIR}/5-ROI-FeatureScout-CylinderVis-WithAutoID.png)
 	set(TEST_NAME "FeatureScoutWithAutoIDTest")
-	set(TEST_COMMAND ${GUITestBinary} ${CMAKE_SOURCE_DIR}/test/data/fibers/5-ROI-FeatureScout-CylinderVis-WithAutoID.iaproj --quit 250 --screenshot ${FeatureScoutWithAutoIDTest_OUTPUT})
+	set(TEST_OUTPUT_FILE ${TEST_OUTPUT_DIR}/${TEST_NAME}.png)
+	set(TEST_COMMAND ${GUITestBinary} ${CMAKE_SOURCE_DIR}/test/data/fibers/5-ROI-FeatureScout-CylinderVis-WithAutoID.iaproj --quit 250 --screenshot ${TEST_OUTPUT_FILE})
 	set(TEST_SCRIPT_NAME "${CMAKE_BINARY_DIR}/${TEST_NAME}.cmake")
 	configure_file(${TEST_TEMPLATE_SCRIPT_FILE} ${TEST_SCRIPT_NAME} @ONLY)
 	add_test(NAME ${TEST_NAME} COMMAND ${CMAKE_COMMAND} -P ${TEST_SCRIPT_NAME})
