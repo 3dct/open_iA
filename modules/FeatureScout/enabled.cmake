27f6cb9fcb85270c6db46c0c63bb01d4904ed439
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2024-07-31 14:52:35 +0200

    FeatureScout: Add test for loading project file with MeanObjects

--- modules/FeatureScout/enabled.cmake
+++ modules/FeatureScout/enabled.cmake
@@ -13,7 +13,6 @@ if (openiA_TESTING_ENABLED)
 	set(TEST_OUTPUT_DIR ${CMAKE_BINARY_DIR}/Testing/Temporary)
 	set(TEST_TEMPLATE_SCRIPT_FILE "${CMAKE_SOURCE_DIR}/test/run_test_with_img_output.cmake.in")
 
-	# FeatureScoutNoAutoIDTest
 	set(TEST_NAME "FeatureScoutNoAutoIDTest")
 	set(TEST_OUTPUT_FILE ${TEST_OUTPUT_DIR}/${TEST_NAME}.png)
 	set(TEST_COMMAND ${GUITestBinary} ${CMAKE_SOURCE_DIR}/test/data/fibers/5-ROI-FeatureScout-LabelVis-NoAutoID.iaproj --quit 250 --screenshot ${TEST_OUTPUT_FILE} --size 1920x1080)
@@ -21,10 +20,16 @@ if (openiA_TESTING_ENABLED)
 	configure_file(${TEST_TEMPLATE_SCRIPT_FILE} ${TEST_SCRIPT_NAME} @ONLY)
 	add_test(NAME ${TEST_NAME} COMMAND ${CMAKE_COMMAND} -P ${TEST_SCRIPT_NAME})
 
-	# FeatureScoutWithAutoIDTest
 	set(TEST_NAME "FeatureScoutWithAutoIDTest")
 	set(TEST_OUTPUT_FILE ${TEST_OUTPUT_DIR}/${TEST_NAME}.png)
 	set(TEST_COMMAND ${GUITestBinary} ${CMAKE_SOURCE_DIR}/test/data/fibers/5-ROI-FeatureScout-CylinderVis-WithAutoID.iaproj --quit 250 --screenshot ${TEST_OUTPUT_FILE} --size 1920x1080)
+	set(TEST_SCRIPT_NAME "${CMAKE_BINARY_DIR}/${TEST_NAME}.cmake")
+	configure_file(${TEST_TEMPLATE_SCRIPT_FILE} ${TEST_SCRIPT_NAME} @ONLY)
+	add_test(NAME ${TEST_NAME} COMMAND ${CMAKE_COMMAND} -P ${TEST_SCRIPT_NAME})
+
+	set(TEST_NAME "FeatureScoutMeanObjects")
+	set(TEST_OUTPUT_FILE ${TEST_OUTPUT_DIR}/${TEST_NAME}.png)
+	set(TEST_COMMAND ${GUITestBinary} ${CMAKE_SOURCE_DIR}/test/data/fibers/5-ROI-FeatureScout-MeanObjects.iaproj --quit 250 --screenshot ${TEST_OUTPUT_FILE} --size 1920x1080)
 	set(TEST_SCRIPT_NAME "${CMAKE_BINARY_DIR}/${TEST_NAME}.cmake")
 	configure_file(${TEST_TEMPLATE_SCRIPT_FILE} ${TEST_SCRIPT_NAME} @ONLY)
 	add_test(NAME ${TEST_NAME} COMMAND ${CMAKE_COMMAND} -P ${TEST_SCRIPT_NAME})
