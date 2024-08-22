a5bca1775b78518be54762d56d0a4129c4604032
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2024-08-22 15:42:46 +0200

    Minor runAsync documentation improvements

--- libs/guibase/iARunAsync.h
+++ libs/guibase/iARunAsync.h
@@ -11,17 +11,18 @@
 //! @param finish the function to run after the main task (runner) is done; it's
 //!         linked via signal-slot mechanism, so this runs in whatever thread
 //!         it "belongs" to
-//! @param context the object in whose context the finish function should run.
-//!         (should? serve two purposes: context provides thread that finish function runs in; and when context deleted, so are connections)
-//!         TODO: STOP THREAD - through future watcher?
-//!         second purpose behind it is that once the object context links to is destroyed,
-//!         so is the connection; used to handle for example the case of terminating
-//!         the application; if you pass a context object here that automatically gets
-//!         destroyed with the application (e.g. some GUI element), then your finished
-//!         handler also gets disconnected; if it's a long-running operation, you
-//!         might need to take other measures that the operation itself is aborted too
-//!         (see e.g. iAJobListView who calls abort on any jobs added to it which have
-//!         an abort listener registered)
+//! @param context the object in whose context the finish function should run. (should?) serve two purposes:
+//!         - context provides thread that finish function runs in; and when context deleted, so are connections)
+//!           TODO: STOP THREAD - through future watcher? - not possible - according to Qt
+//!           documentation, QFutureWatcher returned by QtConcurrent::run does not support cancelling.
+//!         - second purpose behind it is that once the object context links to is destroyed,
+//!           so is the connection; used to handle for example the case of terminating
+//!           the application; if you pass a context object here that automatically gets
+//!           destroyed with the application (e.g. some GUI element), then your finished
+//!           handler also gets disconnected; if it's a long-running operation, you
+//!           might need to take other measures that the operation itself is aborted too
+//!           (see e.g. iAJobListView who calls abort on any jobs added to it which have
+//!           an abort listener registered)
 template <typename RunnerT, typename FinishT>
 QFutureWatcher<void>* runAsync(RunnerT runner, FinishT finish, QObject* context)
 {
